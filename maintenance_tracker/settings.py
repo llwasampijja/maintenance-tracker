720363ccb3f5d6c291f41cd453e09c63637e9691
edna <edna@rr.com>
2019-03-19 17:10:21 +0300

    fix(token): add expiration time [Delivers #164723645]

--- maintenance_tracker/settings.py
+++ maintenance_tracker/settings.py
@@ -121,15 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 JWT_AUTH = {
-   # how long the original token is valid for
-   'JWT_EXPIRATION_DELTA': datetime.timedelta(days=2),
-
-   # allow refreshing of tokens
-   'JWT_ALLOW_REFRESH': True,
-
-   # this is the maximum time AFTER the token was issued that
-   # it can be refreshed.  exprired tokens can't be refreshed.
-   'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+   'JWT_EXPIRATION_DELTA': datetime.timedelta(days=1),
 }
 
 # Internationalization
